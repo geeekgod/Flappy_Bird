f62518b0b962193e197b8d3742839e75139fdb07
Rishabh Singh <ris8645208@gmail.com>
2022-12-24 23:04:38 +0530

    chore: add background & ground script for parallax & movement

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -1,12 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+public class Ground : MonoBehaviour
 {
     private MeshRenderer meshRenderer;
 
     // Start is called before the first frame update
 
-    public float animationSpeed = 0.05f;
+    public float animationSpeed = 0.75f;
 
     private void Awake()
     {
@@ -18,6 +20,6 @@ public class Parallax : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0f);
     }
 }
